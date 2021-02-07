d46270e0d327999b2983a52aca35946e67dc3e8f
Muskan Jaiswal <muskanmahak777@gmail.com>
2021-02-07 21:25:02 +0530

    Tours.js file code done

--- 2-tours/src/Tours.js
+++ 2-tours/src/Tours.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import Tour from './Tour';
+import React from "react";
+import Tour from "./Tour";
 
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({ tours }) => {
+  return (
+    <main>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour}></Tour>;
+        })}
+      </div>
+    </main>
+  );
 };
 
 export default Tours;

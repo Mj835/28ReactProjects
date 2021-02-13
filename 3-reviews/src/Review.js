8192b3eb69f47f8f2907389d7ac9c44c1ca3c303
Muskan Jaiswal <muskanmahak777@gmail.com>
2021-02-13 23:34:57 +0530

    Updated Review.js File
    Now use suprise button to get random review.

--- 3-reviews/src/Review.js
+++ 3-reviews/src/Review.js
@@ -34,7 +34,11 @@ const Review = () => {
 
   const randomPerson = () => {
     let randomNumber = Math.floor(Math.random() * people.length);
-    console.log(randomNumber);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+    // console.log(randomNumber);
   };
 
   return (

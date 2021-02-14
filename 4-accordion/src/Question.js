da8bba491f4f299f11021153b9d7accc5c1b26a6
Muskan Jaiswal <muskanmahak777@gmail.com>
2021-02-14 17:57:25 +0530

    Updated App.js FIle

--- 4-accordion/src/Question.js
+++ 4-accordion/src/Question.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
 const Question = () => {
   return <h2>question component</h2>;
 };

986b47e97afd2c2350c002e2819d44a535d22c3a
Muskan Jaiswal <muskanmahak777@gmail.com>
2021-02-15 23:47:09 +0530

    Update App.js

--- 5-menu/src/App.js
+++ 5-menu/src/App.js
@@ -7,14 +7,18 @@ function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
-  return <main>
-    <section className="menu section">
-      <div className="title">
-        <h2>Our Menu</h2>
-        
-      </div>
-    </section>
-  </main>;
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu />
+      </section>
+    </main>
+  );
 }
 
 export default App;

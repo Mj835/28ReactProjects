aba520d9f7e289186ff2d90b2448766a61c4e5e3
Muskan Jaiswal <muskanmahak777@gmail.com>
2021-02-20 20:24:24 +0530

    Tabs Project Completed
    This Project is made by using useState and useEffect

--- 6-tabs/src/App.js
+++ 6-tabs/src/App.js
@@ -38,11 +38,32 @@ function App() {
       </div>
       <div className="jobs-center">
         {/* Button Container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
         {/* Job Info */}
         <article className="job-info">
           <h3>{title}</h3>
           <h4>{company}</h4>
           <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            );
+          })}
         </article>
       </div>
     </section>
